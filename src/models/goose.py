a3da196e02ca86ee640e492884dc733c94e8eed0
alexssokolov007-oss <alexssokolov007@gmail.com>
2025-12-24 00:28:35 +0300

    Update goose.py

--- src/models/goose.py
+++ src/models/goose.py
@@ -1,10 +1,8 @@
 from src.constants import MERGE_HYPE_BONUS
 from src.models.player import Player
 
-
 class Goose:
     """Базовый гусь с именем и громкостью"""
-
     def __init__(self, name: str, honk_volume: int) -> None:
         self.name = name
         self.honk_volume = honk_volume
@@ -19,18 +17,14 @@ class Goose:
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}(name={self.name!r}, honk_volume={self.honk_volume})"
 
-
 class WarGoose(Goose):
     """Гусь, который атакует игроков"""
-
     def attack(self, player: Player, damage: int) -> int:
         """Атаковать игрока и вернуть фактически нанесенный урон"""
         return player.withdraw(damage)
 
-
 class HonkGoose(Goose):
     """Гусь, который может кричать и пугать игроков"""
-
     def __call__(self, player: Player, impact: int) -> int:
         """Крикнуть на игрока и вернуть величину потерь"""
         return player.withdraw(impact)
